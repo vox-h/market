e89ca2499e5cdebf963c718ee1ed4ab63f65c371
vox-h <76954005+vox-h@users.noreply.github.com>
2025-04-22 23:19:54 +0300

    PostgreSQL db connected & 3 type of search

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
 from goods.models import Products
 from django.db.models import Q
 
@@ -6,12 +8,21 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    # return Products.objects.filter(description__search=query)
 
-    q_objects = Q()
+    # return Products.objects.annotate(search=SearchVector("name", "description")).filter(search=query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    return Products.objects.filter(q_objects)
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    #
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
