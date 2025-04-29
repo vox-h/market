f59102a630a129424e6c51f9cb593f59fa7d6cc8
vox-h <76954005+vox-h@users.noreply.github.com>
2025-04-29 16:25:37 +0300

    login required decorator - redirect to target page

--- app/users/views.py
+++ app/users/views.py
@@ -18,6 +18,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -66,6 +70,7 @@ def profile(request):
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы успешно вышли из аккаунта!")
     auth.logout(request)
