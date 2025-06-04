f05f5d7c05a53c9d7f5b49f0cbfd0252b2ef4baf
vox-h <76954005+vox-h@users.noreply.github.com>
2025-06-04 21:45:45 +0300

    display of orders in the profile and some changes

--- app/orders/views.py
+++ app/orders/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.forms import ValidationError
 from django.db import transaction
 from django.shortcuts import render, redirect
@@ -8,6 +9,7 @@ from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
 
 
+@login_required
 def create_order(request):
     if request.method == 'POST':
         form = CreateOrderForm(data=request.POST)
@@ -66,6 +68,7 @@ def create_order(request):
     context = {
         'title': 'Home - Оформление заказа',
         'form': form,
+        'order': True,
     }
 
     return render(request, 'orders/create_order.html', context=context)
