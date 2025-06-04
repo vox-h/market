dec1726b5b91eb666f017b05c3184960305ba444
vox-h <76954005+vox-h@users.noreply.github.com>
2025-06-04 21:11:36 +0300

    Create order is finish

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
 from users.models import User
 
 # admin.site.register(User)
@@ -9,4 +10,4 @@ from users.models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email')
 
-    inlines = [CartTabAdmin]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
